67912e67d5b8cf35195490202ed6ff0e42748a01
FabioProjects <fabio.thiago3010@gmail.com>
2025-04-11 14:34:38 -0300

    Realização dos Exercícios do PPT

---
+++ TesteAula/src/main/java/Tetse.java
@@ -0,0 +1,15 @@
+public class Tetse {
+    public static void main(String[] args) {
+        int qtdIrmaos = 2;
+        char sexo = 'F';
+        double minhaAltura = 1.65;
+        String meuNome = "Rossi Teixeira";
+        final int MES_NASCIMENTO = 12;
+
+        System.out.println("Olá meu nome é " + meuNome);
+        System.out.println("Sexo " + sexo);
+        System.out.println("Altura " + minhaAltura);
+        System.out.println("Mês de Nascimento " + MES_NASCIMENTO);
+        System.out.println("Quantidade irmãos " + qtdIrmaos);
+    }
+}
